2af75b31dce0ecd65dc5ae2758c63d8d37a4d2e8
Evan Morrison <evan.morrison@skyhubdigital.com>
2017-04-24 16:51:57 +1000

    change yml

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn clean install
-mvn cobertura:cobertura
+#mvn clean install
+#mvn cobertura:cobertura
 bash <(curl -s https://codecov.io/bash) -t 6a3bd938-ddd1-4e59-a381-1585eba8a301
