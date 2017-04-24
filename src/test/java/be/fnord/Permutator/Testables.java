439e4edb5dad2f134f8b6c3a186df35eb671ba31
Evan Morrison <evan.morrison@skyhubdigital.com>
2017-04-24 11:23:54 +1000

    Adding a test

--- src/test/java/be/fnord/Permutator/Testables.java
+++ src/test/java/be/fnord/Permutator/Testables.java
@@ -1,9 +1,24 @@
 package be.fnord.Permutator;
 
+import org.junit.Test;
+
+import be.fnord.Combinations.CombinationGenerator;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Testables {
+
+	@Test
+	public void testCombinator(){
+		CombinationGenerator cr = new CombinationGenerator(15,7);
+		while(cr.hasMore()){
+			int [] i = cr.getNext();
+			for(int ii = 0 ; ii < i.length ; ii++)
+				System.out.print(i[ii] + " ");
+			System.out.println("");
+		}
+	}
+
 
     @When("^I run a permutation over \"([^\"]*)\"$")
     public void i_run_a_permutation_over(String arg1) throws Throwable {
